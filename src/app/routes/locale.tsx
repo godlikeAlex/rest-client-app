a1659485a82f63d258fd237659b856c66acbb9b5
godlikeAlex <godlikedesigner@gmail.com>
2025-09-04 01:37:16 +0500

    fix: use hook useChangeLanguage to update client i18n

--- src/app/routes/locale.tsx
+++ src/app/routes/locale.tsx
@@ -1,9 +1,18 @@
-import { redirect } from 'react-router';
+import { Outlet, redirect } from 'react-router';
 import i18n from '../i18n';
 import type { Route } from './+types/locale';
+import { useChangeLanguage } from 'remix-i18next/react';
 
-export async function loader({ params }: Route.LoaderArgs) {
-  if (!i18n.supportedLngs.includes(params.locale)) {
+export async function loader({ params: { locale } }: Route.LoaderArgs) {
+  if (!i18n.supportedLngs.includes(locale)) {
     return redirect(`/${i18n.fallbackLng}`);
   }
+}
+
+export default function LocaleWrapper({
+  params: { locale },
+}: Route.ComponentProps) {
+  useChangeLanguage(locale);
+
+  return <Outlet />;
 }

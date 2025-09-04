910cf4bc89b5cd781a93fd36d1d6f7833ea6d166
godlikeAlex <godlikedesigner@gmail.com>
2025-09-05 01:26:22 +0500

    chore: add libs for coverage and watch, fix range error tests

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,8 +1,15 @@
 import '@testing-library/jest-dom/vitest';
 
-import { afterEach } from 'vitest';
+import { afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 
 afterEach(() => {
   cleanup();
+});
+
+Range.prototype.getBoundingClientRect = vi.fn();
+Range.prototype.getClientRects = () => ({
+  item: vi.fn(),
+  length: 0,
+  [Symbol.iterator]: vi.fn(),
 });

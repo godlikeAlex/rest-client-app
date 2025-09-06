8820e350174fa05f014f50373ca81b63d13806d8
godlikeAlex <godlikedesigner@gmail.com>
2025-09-06 18:00:00 +0500

    feat: add tests from WaitingResponseSection

--- src/pages/RestClientPage/components/WaitingResponseSection/WaitingResponseSection.tsx
+++ src/pages/RestClientPage/components/WaitingResponseSection/WaitingResponseSection.tsx
@@ -1,13 +1,19 @@
 import { Flex, Image, Title } from '@mantine/core';
-import waitingResponseImage from '@/assets/wating-response.png';
 import { useTranslation } from 'react-i18next';
+
+import waitingResponseImage from '@/assets/wating-response.png';
 
 export default function WaitingResponseSection() {
   const { t } = useTranslation();
 
   return (
     <Flex direction="column" align={'center'}>
-      <Image src={waitingResponseImage} height={350} fit="contain" />
+      <Image
+        src={waitingResponseImage}
+        height={350}
+        fit="contain"
+        alt="Waiting your response"
+      />
       <Title order={4} opacity={0.7}>
         {t('restClient.idleResponseMessage')}
       </Title>

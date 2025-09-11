39c1a9940696f106f9ccab56bdf26a401228e848
godlikeAlex <godlikedesigner@gmail.com>
2025-09-12 01:08:18 +0500

    feat: add HTTPStatusBadge

---
+++ src/components/HttpStatusBadge/index.ts
@@ -0,0 +1,1 @@
+export { default as HttpStatusBadge } from './HttpStatusBadge';
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './Header';
 export * from './Footer';
 export * from './WelcomeBanner';
 export * from './CodeEditor';
+export * from './HttpStatusBadge';

b37b43dfe344a4a5b3900fff4e09f7a700467557
godlikeAlex <godlikedesigner@gmail.com>
2025-09-16 18:41:55 +0500

    feat: add more space to auth layout

--- src/app/layouts/auth-layout.tsx
+++ src/app/layouts/auth-layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router';
 
 export default function AuthLayout() {
   return (
-    <Flex justify="center" align="center" mih="70vh" py="sm" px="sm">
+    <Flex justify="center" align="center" mih="75vh" py="xl" px="md">
       <Box maw="100%" w={450}>
         <Outlet />
       </Box>

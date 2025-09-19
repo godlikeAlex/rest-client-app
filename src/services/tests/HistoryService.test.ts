5f106849941683a358a892fe8cb836fd55df242a
Alexander Kalyupanov <alexander.kalyupanov@mail.ru>
2025-09-19 18:30:20 +0300

    feat: add tests for HistoryService

--- src/services/tests/HistoryService.test.ts
+++ src/services/tests/HistoryService.test.ts
@@ -1,29 +1,52 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { type Mock } from 'vitest';
-import HistoryService from '../HistoryService';
+import HistoryService, { type RequestData } from '../HistoryService';
 
-const { setMock, collectionMock, docMock } = vi.hoisted(() => {
+const requestData = {
+  url: 'https://api.test',
+  method: 'GET',
+  time: 123,
+  duration: 1,
+  requestSize: 11,
+  responseSize: 10,
+  status: 200,
+  error: null,
+  requestData: {
+    headers: [{ key: 'Authorization', value: 'Bearer token', enabled: true }],
+    body: '',
+    queryParams: { q: 'test' },
+  },
+};
+
+const { setMock, collectionMock, docMock, getMock } = vi.hoisted(() => {
   const setMock = vi.fn();
+  const getMock = vi.fn(() => ({
+    forEach: (cb: (doc: { id: string; data: () => RequestData }) => void) => {
+      cb({ id: 'req1', data: () => ({ ...requestData, id: 'req1' }) });
+    },
+  }));
 
   const docMock: Mock = vi.fn(() => ({
     set: setMock,
     collection: vi.fn(() => ({
       doc: docMock,
-      get: vi.fn(),
+      get: getMock,
       collection: collectionMock,
     })),
   }));
 
-  const collectionMock: Mock = vi.mocked(vi.fn(() => ({
-    doc: docMock,
-    get: vi.fn(),
-    collection: vi.fn(() => ({
+  const collectionMock: Mock = vi.mocked(
+    vi.fn(() => ({
       doc: docMock,
       get: vi.fn(),
-      collection: collectionMock,
-    })),
-  })));
-  return { setMock, collectionMock, docMock };
+      collection: vi.fn(() => ({
+        doc: docMock,
+        get: vi.fn(),
+        collection: collectionMock,
+      })),
+    }))
+  );
+  return { setMock, collectionMock, docMock, getMock };
 });
 
 vi.mock('@/services/firebase.server', () => ({
@@ -37,25 +60,8 @@ describe(HistoryService, () => {
     vi.clearAllMocks();
   });
 
-  it('saveUserHistory должен корректно сохранять данные пользователя', async () => {
+  it('saveUserHistory should correct save date', async () => {
     const userId = 'agent007';
-    const requestData = {
-      url: 'https://api.test',
-      method: 'GET',
-      time: 123,
-      duration: 1,
-      requestSize: 11,
-      responseSize: 10,
-      status: 200,
-      error: null,
-      requestData: {
-        headers: [
-          { key: 'Authorization', value: 'Bearer token', enabled: true },
-        ],
-        body: '',
-        queryParams: { q: 'test' },
-      },
-    };
 
     await HistoryService.saveUserHistory(userId, requestData);
 
@@ -63,5 +69,15 @@ describe(HistoryService, () => {
     expect(docMock).toHaveBeenCalledWith(userId);
     expect(docMock().collection().doc).toHaveBeenCalledWith();
     expect(setMock).toHaveBeenCalledWith(requestData);
+  });
+
+  it('getUserHistory should correct return user history', async () => {
+    const userId = 'agent007';
+
+    const result = await HistoryService.getUserHistory(userId);
+
+    expect(collectionMock).toHaveBeenCalledWith('users');
+    expect(getMock).toHaveBeenCalledWith();
+    expect(result).toEqual([{ ...requestData, id: 'req1' }]);
   });
 });

dd55ccab60b244e3544192e45bafeaf8bb035e3a
kateNEO <glebova.caterina2012@yandex.ru>
2025-09-21 17:25:29 +0200

    feat: adding footer test

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,5 +1,28 @@
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
+import { screen } from '@testing-library/react';
+import Footer from './Footer';
+import { renderWithProviders } from '@/tests/utils';
 
-describe.todo('footer component', () => {
-  it.todo('need to render img');
+describe('footer component', () => {
+  it('should render logo, contributors and copyright text', () => {
+    expect.hasAssertions();
+
+    renderWithProviders(<Footer />);
+
+    const rsLogo = screen.getByAltText('rs-school');
+
+    expect(rsLogo).toBeInTheDocument();
+
+    const contributors = [
+      'Aleksandr Yurkovskiy',
+      'Alexander Kalyupanov',
+      'Ekaterina Naumenko',
+    ];
+
+    contributors.forEach((name) => {
+      expect(screen.getByText(name)).toBeInTheDocument();
+    });
+
+    expect(screen.getByText('© 2025 Rest Client')).toBeInTheDocument();
+  });
 });
